d9a2e74f701d833e6cf378f0300bb3cce1befc63
Elaine Rodrigues Cazetta <elaine.rodrigues1@gmail.com>
2025-11-05 21:34:03 +0000

    create file stocks.py

---
+++ stocks.py
@@ -0,0 +1,20 @@
+#! usr/bin/env python
+
+# Dates and times
+import datetime as dt
+
+# Yahoo Finance data
+import yfinance as yf
+
+# Download FAANG stocks data:
+df = yf.download(['META', 'AAPL', 'AMZN', 'NFLX', 'GOOG'], period='5d', interval='1h')
+
+# Current date and time
+now = dt.datetime.now()
+
+# File name
+filename = "data/" + now.strftime("%Y%m%d-%H%M%S") + ".csv"
+
+# Save data as CSV
+# Resources: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_csv.html
+df.to_csv(filename)
